0aef0b02c0d51200fa9e01e9fbda821e1cdd4034
Adam Carless <adam.carless@bjss.com>
2023-02-14 09:22:39 +0000

    minor var rename

--- src/main/SlidingArrayRounds.java
+++ src/main/SlidingArrayRounds.java
@@ -4,11 +4,11 @@ public class SlidingArrayRounds implements RoundsInterface {
 
   @Override
   public String getRounds(double score) {
-    int normalised = scoreToInt(score);
-    return DOTS.substring(10 - normalised, 20 - normalised);
+    int rank = scoreToRank(score);
+    return DOTS.substring(10 - rank, 20 - rank);
   }
 
-  private int scoreToInt(double score) {
+  private int scoreToRank(double score) {
     assert score >= 0.0 && score <= 1.0 : "score should be 0.0-1.0";
     return (int) Math.ceil(score * 10);
   }
